facc0b8d14fd2a67532932cfaa33d890c20f8a66
Sergey Komarov <sergey.komarov@oxagile.com>
2017-11-16 15:11:01 +0300

    Added UI elements

--- SimpleProject/ViewController.swift
+++ SimpleProject/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var userEmailField: UITextField!
+    @IBOutlet weak var error: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        userNameField.delegate = self
+        userEmailField.delegate = self
+        error.text = ""
     }
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userNameField.resignFirstResponder()
+        userEmailField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: Actions
+    @IBAction func sentRequest(_ sender: UIButton) {
+        let name = userNameField.text
+        let email = userEmailField.text
+        guard name != "" else {
+            error.text = "Please enter your name"
+            return
+        }
+        guard email != "" else {
+            error.text = "Please enter your email"
+            return
+        }
+        error.text = "\(name!) has succesfully registered"
+        error.textColor = UIColor(red: 0.0, green: 0.6, blue: 0.3, alpha: 1.0)
+    }
+    
 }
-
